c62eee96e3d41e6c635828e14ec8aac08a0cbb65
Samson D. Recluta Jr <92423779+ssr0016@users.noreply.github.com>
2023-09-07 12:59:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ app.use(express.json());
 
 // Middle for handling CORS POLICY
 // Option 1: Allow All Origins with Default of cors(*)
-// app.use(cors());
+app.use(cors());
 //option 2: allow Custom Origins
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
 
 //routes
 app.get('/', (request, response)=>{
